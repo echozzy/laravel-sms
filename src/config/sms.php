4b42a807ea2f6a377032a8817ea8d6d5fe676dd2
echozzy <348858954@qq.com>
2019-11-05 11:53:14 +0800

    sms

--- src/config/sms.php
+++ src/config/sms.php
@@ -27,20 +27,28 @@ return [
             'access_key_id' => '',
             'access_key_secret' => '',
             'sign_name' => '',
+            'gateway_name' => '阿里云',
+            'template' => [
+                'sms_code' => 'SMS_176910465',//模版标识和编号
+                'sms_deliver_goods' => '',
+            ]
         ],
         'juhe' => [
             'app_key' => '',
+            'gateway_name' => '聚合数据',
         ],
         'baidu' => [
             'ak' => '',
             'sk' => '',
             'invoke_id' => '',
             'domain' => '',
+            'gateway_name' => '百度云',
         ],
         'qcloud' => [
             'sdk_app_id' => '', // SDK APP ID
             'app_key' => '', // APP KEY
             'sign_name' => '', // 短信签名，如果使用默认签名，该字段可缺省（对应官方文档中的sign）
+            'gateway_name' => '腾讯云 SMS',
         ],
     ],
 ];
